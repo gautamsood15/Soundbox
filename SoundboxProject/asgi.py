a953525dbc07218bacd6cca8ea939bd5f10f65e6
guatamsood15 <gauty22@gmail.com>
2022-08-18 22:03:33 +0530

    Adding vercel in django

--- SoundboxProject/asgi.py
+++ SoundboxProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Soundbox project.
+ASGI config for SoundboxProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Soundbox.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SoundboxProject.settings")
 
 application = get_asgi_application()
--- SoundboxProject/urls.py
+++ SoundboxProject/urls.py
@@ -1,4 +1,4 @@
-"""Soundbox URL Configuration
+"""SoundboxProject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
