f6545fa8a3f7393ca4db9ad8cec0ea836f572283
guatamsood15 <gauty22@gmail.com>
2022-08-18 22:33:02 +0530

    Adding new code

--- SoundboxProject/settings.py
+++ SoundboxProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j#e&-k%(+v&s#@(72#s)qz$@@u8j=1zvu@_a4c#laq67!1$kl6"
+SECRET_KEY = "django-insecure-@_)nef!datzrenjx&^vmzp(sq_!ac&lxwr@v_9r9jo-pyh_8c-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
