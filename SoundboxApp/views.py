32d9e51bfbf1f79007204692d72ea89d9bee6d47
guatamsood15 <gauty22@gmail.com>
2022-09-02 05:10:08 +0530

    Adding JS code to the frontend for music player

--- SoundboxApp/views.py
+++ SoundboxApp/views.py
@@ -4,5 +4,6 @@ from .models import Music
 # Create your views here
 def homePage(request):
     music = Music.objects.all()
-    context = {'music': music}
+    music_list=list(Music.objects.all().values())
+    context = {'music': music, 'music_list': music_list}
     return render(request, "home.html", context)
