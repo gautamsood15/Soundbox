0a7921ca6af4dc72285f652d9e67534cbbcc082b
guatamsood15 <gauty22@gmail.com>
2022-09-02 01:19:09 +0530

    Adding templatetags, get_audio_length, validate_is_audio,

--- SoundboxApp/admin.py
+++ SoundboxApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Music, Album
 
 # Register your models here.
+
+admin.site.register(Music)
+admin.site.register(Album)
--- SoundboxApp/models.py
+++ SoundboxApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .helpers import get_audio_length
+from .validators import validate_is_audio
 
 # Create your models here.
 
@@ -7,13 +9,16 @@ class Music(models.Model):
       artist=models.CharField(max_length=500)
       album=models.ForeignKey('Album', null=True, blank=True, on_delete=models.SET_NULL)
       time_length=models.DecimalField(blank=True, max_digits=20, decimal_places=2)
-      audio_file=models.FileField(upload_to="musics")
+      audio_file=models.FileField(upload_to="musics", validators=[validate_is_audio])
       cover_image=models.ImageField(upload_to="music_image/")
 
       def save(self, *args, **kwargs):
         if not self.time_length:
             # logic for getting music length here
-            pass
+            
+            audio_length=get_audio_length(self.audio_file)
+            self.time_length=audio_length
+
         return super().save(*args, **kwargs)
 
 
