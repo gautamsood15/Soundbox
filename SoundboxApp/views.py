1c411057d8634dcc31035a293e95e46c9c7e250c
guatamsood15 <gauty22@gmail.com>
2022-09-02 23:59:23 +0530

    Final commit :)

--- SoundboxApp/views.py
+++ SoundboxApp/views.py
@@ -1,6 +1,6 @@
 from SoundboxApp.form import AddMusicForm
 from django.shortcuts import render
-from .models import Music
+from .models import Music, Album
 
 # Create your views here
 def homePage(request):
@@ -12,5 +12,17 @@ def homePage(request):
 
 def addMusic(request):
     form=AddMusicForm()
+    if request.POST:
+        form=AddMusicForm(request.POST, request.FILES)
+        if form.is_valid():
+            instance=form.save(commit=False)
+            album=form.cleaned_data.get('album')
+            if album:
+                music_album=Album.object.get_or_create(name=album)
+                instance.album=music_album[0]
+                instance.save()
+            else:
+                instance.save()
+            return redirect("music:home_page")
     context = {'form': form}
     return render(request, "addPage.html", context)
